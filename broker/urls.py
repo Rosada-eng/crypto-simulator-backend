863af16120612d6c3d56978581718c479acd1f8e
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-11-05 10:36:23 -0300

    modifica edit_profile para devolver o user modificado

--- broker/urls.py
+++ broker/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     #<> Rotas para API:
     # Retorna as transações de um usuário
     path('broker/<int:user_id>/', views.get_trades, name='get_trades_from_user'),
+    
     # Faz novas transações -- Dados necessários são passados pelo body do request (userId, cryptoId, unit_price, quantity, unit)
-    path('broker/')
+    path('broker/', views.make_trade, name='new_trade')
 ]
