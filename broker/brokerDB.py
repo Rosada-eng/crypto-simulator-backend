a9849099b8f1f3c45866eeacedf5ab12137c49fc
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-11-13 18:28:50 -0300

    Ajusta para so realizar venda se o usuario tiver quantidade suficiente da moeda

--- broker/brokerDB.py
+++ broker/brokerDB.py
@@ -26,7 +26,7 @@ def new_trade (user_id, crypto_id, crypto_name, unit_price, quantity, unit:str='
     user = User.objects.get(id=user_id)
     user_current_money = user._check_money_amount()
 
-    if user_current_money >= float(unit_price) * float(quantity):
+    if operation == 'buy' and (user_current_money >= float(unit_price) * float(quantity)):
         trade = Broker.objects.create(
             user = user,
             crypto_id = crypto_id, 
@@ -40,5 +40,32 @@ def new_trade (user_id, crypto_id, crypto_name, unit_price, quantity, unit:str='
         user.current_money -= float(unit_price) * float(quantity)
         user.save()
         return trade
+    #! OBS.: Vendas possuem quantity negativas!!
+    elif operation == 'sell':
+        user_trades = Broker.objects.filter(user_id = user_id, crypto_id = crypto_id)
+        user_crypto_amount = 0
+
+        for trade in user_trades:
+            print(trade)
+            user_crypto_amount += trade.quantity
+
+        print("crypto_amount: ", user_crypto_amount)
+        if user_crypto_amount >= float(abs(quantity)):
+            trade = Broker.objects.create(
+            user = user,
+            crypto_id = crypto_id, 
+            crypto_name = crypto_name,
+            operation = operation,
+            unit_price = unit_price, 
+            unit = unit, 
+            quantity = quantity
+        )
+
+            user.current_money -= float(unit_price) * float(quantity)
+            user.save()
+            return trade
+        else:
+            raise Exception('You dont have enough to sell')
+
     else:
         raise Exception('Money amount is not enough')
